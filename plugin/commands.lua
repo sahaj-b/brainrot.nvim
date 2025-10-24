c5840517a1f954697ca1abc00d89998c5a5f1dcc
sahaj-b <sahajb0606@gmail.com>
2025-10-24 23:50:27 +0530

    :Brainrot subcommands

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -1,7 +1,17 @@
-vim.api.nvim_create_user_command('Phonk', function()
-  require('brainrot').phonk()
-end, {})
+vim.api.nvim_create_user_command('Brainrot', function(opts)
+  local subcommand = opts.args
+  local brainrot = require('brainrot')
 
-vim.api.nvim_create_user_command('Boom', function()
-  require('brainrot').boom()
-end, {})
+  if subcommand == 'phonk' then
+    brainrot.phonk()
+  elseif subcommand == 'boom' then
+    brainrot.boom()
+  else
+    vim.notify('Invalid subcommand: ' .. subcommand .. '\nUsage: :Brainrot <phonk|boom>', vim.log.levels.ERROR)
+  end
+end, {
+  nargs = 1,
+  complete = function()
+    return { 'phonk', 'boom' }
+  end
+})
